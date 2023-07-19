3396b6c4c3960af6e9366c58d16b9803aac35b24
dansolution <danielachowue@gmail.com>
2023-07-19 21:53:05 +0100

    day1_Exercise.js

---
+++ day1_Exercise.js
@@ -0,0 +1,83 @@
+
+//  QUESTION 1
+
+// can make code readablE
+
+//  QUESTION 2
+
+//Welcome to 30DaysOfJavaScript
+
+//  QUESTION 3
+/*comments can make code readable, easy to reuse and informative */
+
+
+//  QUESTION 4
+
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let myString ="This is my first assignment"
+let myBoolean= true;
+let myUndefined= undefined;
+let myNull  = null;
+
+//  QUESTION 5
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof myString);
+console.log(typeof myBoolean);
+console.log(typeof myUndefined);
+console.log(typeof myNull);
+
+
+//  QUESTION 6
+//Declare four variables without assigning values
+
+let myVariables1;
+let myVariables2;
+let myVariables3;
+let myVariables4;
+
+
+//  QUESTION 7
+
+//Declare four variables with assigned values
+ 
+let isString = "learning 30 days of javascript";
+let isNum  = 12345;
+let isBoolean = true;
+let isNull = null
+
+//  QUESTION 8
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let myFirstName ="Daniel"
+let myLastName ="Achowue"
+let myMaritalStatus ="married"
+let myCountry = "Nigeria"
+let Age = 30
+
+
+//  QUESTION 9
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstName ="Daniel",lastName ="Achowue",maritalStatus ="married",country="Nigeria",age=30;
+
+
+//  QUESTION 10
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+//I am 25 years old.
+//You are 30 years old.
+
+
+
+const myAge = 25;
+const yourAge = 35;
+
+console.log(`I am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
+
+
+
+
